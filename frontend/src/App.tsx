21790b37a7e175529e35cbb642553763fdfb6e67
Felipe Gomes <felipegstech@gmail.com>
2026-03-05 13:12:10 -0300

    feat: implement protected routes in front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Index from "../src/pages/Index";
 import Login from "../src/pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 import { Toaster } from "@/components/ui/toaster";
 
 function App() {
@@ -13,7 +14,14 @@ function App() {
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
